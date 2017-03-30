5858f2ef9ea43ce277f4c6771eafb2eba5ba8dc3
justinbavier <jbavier123@gmail.com>
2017-03-30 17:18:22 -0500

    spitStuffOut

--- cloud/main.js
+++ cloud/main.js
@@ -4,10 +4,16 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 
-/*Parse.Cloud.define('hello', function(req, res) {
+Parse.Cloud.define('hello', function(req, res) {
   const email = req.params.email;
   const charity = req.params.charity;
   const firstName = req.params.firstName;
   const lastName = req.params.lastName;
 });
-*/
+
+Parse.Cloud.define('storeEmailName', function(req, res) {
+  const email = req.params.email;
+  const firstName = req.params.firstName;
+  const lastName = req.params.lastName;
+  res.success(email + " " + firstName + " " + lastName);
+});
