7e60b29743962855f7aefe086caa3980dc39cee5
John Ottenlips <johno@aloompa.com>
2017-03-30 14:09:36 -0500

    getting info from params

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,11 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+
+Parse.Cloud.define('hello', function(req, res) {
+  const email = req.params.email;
+  const charity = req.params.charity;
+  const firstName = req.params.firstName;
+  const lastName = req.params.lastName;
+});
