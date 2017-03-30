577871eb873b07d49ba781d5e8ea06ea9e231b9f
justinbavier <jbavier123@gmail.com>
2017-03-30 17:14:38 -0500

    hello

--- cloud/main.js
+++ cloud/main.js
@@ -4,9 +4,10 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 
-Parse.Cloud.define('hello', function(req, res) {
+/*Parse.Cloud.define('hello', function(req, res) {
   const email = req.params.email;
   const charity = req.params.charity;
   const firstName = req.params.firstName;
   const lastName = req.params.lastName;
 });
+*/
