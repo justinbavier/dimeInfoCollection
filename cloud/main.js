d3713da8d238a5e0e178a6811842f5f7c9bf9ab6
justinbavier <jbavier123@gmail.com>
2017-03-30 17:50:25 -0500

    Please lord

--- cloud/main.js
+++ cloud/main.js
@@ -16,19 +16,19 @@ Parse.Cloud.define('storeEmailName', function(req, res) {
   // const firstName = req.params.firstName;
   // const lastName = req.params.lastName;
 
-  var Person = Parse.Object.extend('Shucks');
+  var Person = Parse.Object.extend('Person');
   var newPerson = new Person();
 
-  newPerson.save({
-    email: 'jbavier123@gmail.com',
-    firstName: 'Justin',
-    lastName: 'Bavier'
-  }, {
-    success: function(newPerson) {
+  newPerson.set("email", "email");
+  newPerson.set("firstName", "firstName");
+  newPerson.set("lastName", "lastName");
 
+  newPerson.save(null, {
+    success: function(item) {
+      console.log("Good");
     },
-    error: function(newPerson, error) {
-
+    error: function(item, error) {
+      console.log("Bad");
     }
   });
 });
