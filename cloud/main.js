f686f6f624928ead66a66032be81b3af03dca769
John Ottenlips <johno@aloompa.com>
2017-03-31 16:12:52 -0500

    Added SERVER_URL

--- cloud/main.js
+++ cloud/main.js
@@ -5,29 +5,34 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 
-Parse.Cloud.define('hello', function(req, res) {
-  const email = req.params.email;
-  const charity = req.params.charity;
-  const firstName = req.params.firstName;
-  const lastName = req.params.lastName;
-});
+// Parse.Cloud.define('hello', function(req, res) {
+//   const email = req.params.email;
+//   const charity = req.params.charity;
+//   const firstName = req.params.firstName;
+//   const lastName = req.params.lastName;
+// });
 
 Parse.Cloud.define('storeEmailName', function(req, res) {
   // const email = req.params.email;
   // const firstName = req.params.firstName;
   // const lastName = req.params.lastName;
 
-  Parse.initialize("User Info Collection Server");
 
-  var Test = new Parse.Object.extend("Test");
-  var newTest = new Test();
+  var GameScore = Parse.Object.extend("Person");
+  var gameScore = new GameScore();
 
-  newTest.set("name", "Test");
-  newTest.set("why", "because");
-  newTest.save().then(function(user){
-    response.success('Save successful');
-  }, function(error){
-    console.error(error);
-    response.error(error);
-  })
+  gameScore.set("email", 1337);
+  gameScore.set("firstName", "Sean Plott");
+
+  gameScore.save(null, {
+    success: function(gameScore) {
+      // Execute any logic that should take place after the object is saved.
+      alert('New object created with objectId: ' + gameScore.id);
+    },
+    error: function(gameScore, error) {
+      // Execute any logic that should take place if the save fails.
+      // error is a Parse.Error with an error code and message.
+      alert('Failed to create new object, with error code: ' + error.message);
+    }
+  });
 });
